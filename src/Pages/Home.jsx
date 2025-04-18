c593d48cce2e9201991a912e14caa1347596bcfb
mhasaero <eroouji7314@gmail.com>
2025-04-18 07:31:00 +0700

    feat: add routing and Home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import Nav from "../Components/Nav";
+
+export default function Home() {
+  return (
+    <section id="home">
+      <Nav />
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./Pages/Home";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
